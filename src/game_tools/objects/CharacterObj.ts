9b73752220945530c0c6d25efaa675bf57d44167
shawn-dev-5790 <shawn.dev.5790@gmail.com>
2023-07-01 20:51:23 +0900

    fix: :bug: 컴파일 에러 수정중

--- src/game_tools/objects/CharacterObj.ts
+++
@@ -1,8 +0,0 @@
-import CharacterEntity from '../../shared/entities/CharacterEntity'
-import { GameObject } from '../GameObjectPool'
-
-export class CharacterObj extends CharacterEntity {
-  position: any = {}
-  movement: any = {}
-  game_object: GameObject = new GameObject()
-}
